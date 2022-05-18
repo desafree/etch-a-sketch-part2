0715768f4f6eec6fcc80c0661512a9afa6a722c5
desafree <nicoladesanctis99@gmail.com>
2022-05-18 17:14:46 +0200

    add features, missing optimization and style

--- script.js
+++ script.js
@@ -30,11 +30,11 @@ const box = (function () {
     boxContainer.remove();
   };
 
-  box.addListeners = function (color = "#000000") {
+  box.addListeners = function (callback) {
     const cells = document.querySelectorAll(".cell");
     cells.forEach((cell) => {
       cell.addEventListener("mouseenter", (e) => {
-        e.target.style.backgroundColor = color;
+        e.target.style.backgroundColor = callback();
       });
     });
   };
@@ -42,26 +42,75 @@ const box = (function () {
   return box;
 })();
 
+const pen = (function () {
+  let GRADIENT = 0;
+  let RAINBOW = 0;
+  let color = "#000000";
+  let times = 0;
+
+  function randomColor() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  let obj = {};
+  obj.getColor = function () {
+    if (RAINBOW == 1) {
+      color = randomColor();
+    }
+    if (GRADIENT == 1) {
+      times++;
+      if (times > 100) {
+        times = 0;
+      }
+      color = `hsl(100, 0%,${times}%`;
+    }
+    return color;
+  };
+
+  obj.reset = function () {
+    GRADIENT = 0;
+    RAINBOW = 0;
+  };
+
+  obj.setGradient = function () {
+    obj.reset();
+    GRADIENT = 1;
+  };
+
+  obj.setRainbow = function () {
+    obj.reset();
+    RAINBOW = 1;
+  };
+
+  obj.setBlack = function () {
+    obj.reset();
+    color = "#000000";
+  };
+
+  return obj;
+})();
+
 box.generateBox(16);
-box.addListeners();
+box.addListeners(pen.getColor);
 
 const range = document.querySelector("input[type=range]");
 range.addEventListener("change", (e) => {
   box.removeBox();
   box.generateBox(Number(e.target.value));
-  box.addListeners();
+  box.addListeners(pen.getColor);
 });
 
-const pen = (function () {
-  let BLACK = 1;
-  let GRADIENT = 0;
-  let RAINBOW = 0;
-  let color = "#ffffff";
+const blackOption = document.querySelector(".black");
+blackOption.addEventListener("click", () => {
+  pen.setBlack();
+});
 
-  let obj = {};
-  obj.getColor = function () {
-    return color;
-  };
+const gradientOption = document.querySelector(".gradient");
+gradientOption.addEventListener("click", () => {
+  pen.setGradient();
+});
 
-  return obj;
-})();
+const rainbowOption = document.querySelector(".rainbow");
+rainbowOption.addEventListener("click", () => {
+  pen.setRainbow();
+});
